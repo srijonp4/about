79aefb5f2d6e3e5ba092f2de9450432faa217253
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2024-10-29 23:47:46 +0530

    initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ import {
   LinkedInLogoIcon,
 } from '@radix-ui/react-icons';
 
-type Props = {};
+// type Props = {};
 
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <footer className="mb-16 ">
       <ul className=" font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
